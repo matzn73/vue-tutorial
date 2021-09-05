7683f32a7f55856a5253b580d54329bab2074f5e
Yuta Matsumoto <matsumoto@biophilia.co.jp>
2021-09-05 22:05:25 +0900

    finish

---
+++ main.js
@@ -0,0 +1,73 @@
+var STORAGE_KEY = 'todos-vuejs-demo'
+var todoStorage = {
+  fetch: function () {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    todos.forEach(function (todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function (todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    todos: [],
+    options: [
+      { value: -1, label: 'All' },
+      { value: 0, label: 'Doing' },
+      { value: 1, label: 'Done' },
+    ],
+    current: -1
+  },
+  methods: {
+    doAdd: function (event, value) {
+      var comment = this.$refs.comment
+      if (!comment.value.length) {
+        return
+      }
+      this.todos.push({
+        id: todoStorage.uid++,
+        comment: comment.value,
+        state: 0
+      })
+      comment.value = ''
+    },
+    doChangeState: function (item) {
+      item.state = item.state ? 0 : 1
+    },
+    doRemove: function (item) {
+      var index = thid.todos.indexOf(item)
+      this.todos.splice(index, 1)
+    }
+  },
+  watch: {
+    todos: {
+      handler: function (todos) {
+        todoStorage.save(todos)
+      },
+      deep: true
+    }
+  },
+  created() {
+    this.todos = todoStorage.fetch()
+  },
+  computed: {
+    labels() {
+      return this.options.reduce(function (a, b) {
+        return Object.assign(a, { [b.value]: b.label })
+      }, {})
+    },
+    computedTodos: function () {
+      return this.todos.filter(function (el) {
+        return this.current < 0 ? true : this.current == el.state
+      }, this)
+    }
+  }
+})
